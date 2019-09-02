fbdd314af8ae0f69e2b795f995e64bedf260dec9
biagiola <41311088+biagiola@users.noreply.github.com>
2019-09-02 11:34:00 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,62 @@
 const cafeList = document.querySelector('#cafe-list');
 const form = document.querySelector('#add-cafe-form');
 
-// bring up the data from firebase
-db.collection('clientes').get().then( (snapshot) => {
-    snapshot.docs.forEach( doc => {
-        renderCafe(doc);
-        console.log(doc.data().nombre);
-    })
-})
-
 // create element and render cafe
 function renderCafe(doc) {
+    // we create the elements
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
     let cross = document.createElement('span');
     
-    li.setAttribute('id', doc.id);
-    cross.setAttribute('id', doc.id);
-    li.style.display = "none";
+    // add the unique id
+    li.setAttribute('data-id', doc.id);
     
+    // add the data in the span tag
     name.textContent = doc.data().nombre + ' ';
     city.textContent = doc.data().ciudad;
     cross.textContent = 'x';
     cross.style.cursor = "pointer";
-    
+
+    //append the span tags to the li tag
     li.appendChild(name);
     li.appendChild(city);
     li.appendChild(cross);
 
+    // append the li tags to the ul tag
     cafeList.appendChild(li);   
+    
+    // add an effect to each li with jquery library
     let a = document.getElementById(doc.id);
     $(a).hide().fadeIn(1000);
     
-
     // deleting data
-    cross.addEventListener('click', (e) => {
+/*     cross.addEventListener('click', (e) => {
         ul.li.style.animationPlayState = "running";
-    });
-
+    });  */
     cross.addEventListener('click', (e) => {
         e.stopPropagation;
-        let id = e.target.parentElement.getAttribute('id');
-        db.collection('cafes').doc(id).delete();
+        let id = e.target.parentElement.getAttribute('data-id'); // we get the id in the parent attribute
+        db.collection('clientes').doc(id).delete();
     });
 }
 
+// getting data 
+db.collection('clientes').get().then( (snapshot) => {
+    snapshot.docs.forEach( doc => {
+        renderCafe(doc);
+        console.log(doc.data());
+    })
+})
+
 // saving data
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    event.preventDefault(); // avoid to refresh the page while we look the db firestore
     db.collection('clientes').add({
         nombre: form.name.value,
         ciudad: form.city.value
     });
-    //clean up again for charging
+    //clean up again for charging again
     form.name.value = '';
     form.city.value = '';
 })
